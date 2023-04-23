252d262c7540e022e526e04b72bb72393a68d220
meganma <adrianlam@g.ucla.edu>
2023-04-22 18:32:18 -0700

    Initial Commit

---
+++ sunset2.0/sunset2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  sunset2_0App.swift
+//  sunset2.0
+//
+//  Created by Megan Ma on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct sunset2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
